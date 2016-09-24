6bf12d63c7a9d6827ff965956f6fca35c7c8c1e5
Deepkeng <346755353@qq.com>
2016-09-24 12:19:28 +0800

    封装databean

---
+++ app/src/main/java/com/pxmao/king/tamperweinxindb/DataBean.java
@@ -0,0 +1,26 @@
+package com.pxmao.king.tamperweinxindb;
+
+/**
+ * Created by psq on 2016/9/22
+ */
+public class DataBean {
+
+    public String nickname;
+    public String remark;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
